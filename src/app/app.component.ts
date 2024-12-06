713a2f2e4c018f505c72c6e686ed3872ac80b8c0
ckchennala <kiranchennala24@gmail.com>
2024-12-05 19:08:53 -0800

    Update app.component.ts
    update text sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}} from angular my-first-static-web-app </div>`,
 })
 export class AppComponent {
   value = 'World';
