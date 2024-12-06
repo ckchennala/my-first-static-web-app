f9854f32d3ecf1215c1075b44950e470e3aa2745
ckchennala <kiranchennala24@gmail.com>
2024-12-05 19:38:43 -0800

    Update app.component.ts
    api message integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}} from angular my-first-static-web-app </div>`,
+  template: `<div>Hello {{value}} from angular !!!!!  Message form {{message}} </div>`,
 })
 export class AppComponent {
   value = 'World';
+  message = 'default';
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
 }
